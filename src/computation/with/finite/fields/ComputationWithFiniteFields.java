ad32cf3cebc295e0131ded44754b894bdfc48a10
Jakub <Jakub@Kubo>
2014-02-02 11:20:24 +0100

    First commit

---
+++ src/computation/with/finite/fields/ComputationWithFiniteFields.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package computation.with.finite.fields;
+
+/**
+ *
+ * @author Jakub
+ */
+public class ComputationWithFiniteFields {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
