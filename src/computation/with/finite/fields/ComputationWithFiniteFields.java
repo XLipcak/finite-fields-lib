ce9c323a1c499040d40479db95aba7420ef69c61
Jakub <Jakub@Kubo>
2014-02-06 21:22:46 +0100

    GF2N + interfaces

--- src/computation/with/finite/fields/ComputationWithFiniteFields.java
+++ src/computation/with/finite/fields/ComputationWithFiniteFields.java
@@ -3,7 +3,10 @@
  * and open the template in the editor.
  */
 package computation.with.finite.fields;
-import computation.with.finite.fields.computation.GaloisField;
+import computation.with.finite.fields.library.GF2N;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Vector;
 
 /**
  *
@@ -15,14 +18,19 @@ public class ComputationWithFiniteFields {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        GaloisField gf = new GaloisField();
-        //long addResult = gf.subtract(47,64);
-        long result = gf.multiply(15,22);
+        /*long lStartTime = System.currentTimeMillis();
+        long lEndTime = System.currentTimeMillis();
+        long difference1 = lEndTime - lStartTime;*/
         
-      //  long inverse = gf.invert(44);
+        long poly = 115;
+        long a = 27;
+        long b = 64;
+        GF2N gf = new GF2N(poly);
         
         
-       // System.out.println(gf.getFieldSize());
-        System.out.println(result);
+        for(int x = 0; x < 64; x++){
+            System.out.println("Inverse" + gf.invert(x) + "   Power: " + gf.power((long)x, (long)Math.pow(2, 6)-2));
+        }
+
     }
 }
