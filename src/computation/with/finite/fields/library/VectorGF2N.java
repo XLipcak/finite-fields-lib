a2a18a1ab7287d6693c6e9064e782c7ddec88431
Jakub <Jakub@Kubo>
2014-02-12 18:56:13 +0100

    vector arithmetic implementation

--- src/computation/with/finite/fields/library/VectorGF2N.java
+++ src/computation/with/finite/fields/library/VectorGF2N.java
@@ -12,21 +12,56 @@ import java.util.List;
  *
  * @author Jakub Lipcak, Masaryk University
  */
-public class VectorGF2N implements GaloisFieldVectorArithmetic{
+public class VectorGF2N implements GaloisFieldVectorArithmetic {
+
+    GF2N galoisField;
+
+    public VectorGF2N(GF2N galoisField) {
+        this.galoisField = galoisField;
+    }
+
+    public VectorGF2N(long reducingPolynomial) {
+        galoisField = new GF2N(reducingPolynomial);
+    }
 
     @Override
     public ArrayList<Long> add(List<Long> vector1, List<Long> vector2) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+
+        isValid(vector1, vector2);
+
+        if (vector1.size() != vector2.size()) {
+            throw new IllegalArgumentException("Vectors must have the same length.");
+        }
+
+        ArrayList<Long> result = new ArrayList<>();
+
+        for (int x = 0; x < vector1.size(); x++) {
+            long value = galoisField.add(vector1.get(x), vector2.get(x));
+            result.add(value);
+        }
+
+        return result;
     }
 
     @Override
     public ArrayList<Long> subtract(List<Long> vector1, List<Long> vector2) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        isValid(vector1, vector2);
+        return add(vector1, vector2);
     }
 
     @Override
     public ArrayList<Long> multiply(List<Long> vector, long scalarValue) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+
+        isValid(vector);
+
+        ArrayList<Long> result = new ArrayList<>();
+
+        for (int x = 0; x < vector.size(); x++) {
+            long value = galoisField.multiply(vector.get(x), scalarValue);
+            result.add(value);
+        }
+
+        return result;
     }
 
     @Override
@@ -38,5 +73,20 @@ public class VectorGF2N implements GaloisFieldVectorArithmetic{
     public int compare(List<Long> vector1, List<Long> vector2) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    
+
+    private void isValid(List<Long> vect1, List<Long> vect2) {
+
+        if (vect1.isEmpty() || vect2.isEmpty()) {
+            throw new IllegalArgumentException("Vector argument is empty.");
+        }
+
+    }
+
+    private void isValid(List<Long> vect) {
+
+        if (vect.isEmpty()) {
+            throw new IllegalArgumentException("Vector argument is empty.");
+        }
+
+    }
 }
